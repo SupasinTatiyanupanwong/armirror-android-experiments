a80ae273c972fff6c59b91fd95a14776e7632f80
Supasin Tatiyanupanwong <supasin@tatiyanupanwong.me>
2018-08-25 19:46:54 +0700

    Handle camera runtime permission

--- mobile/src/main/java/me/tatiyanupanwong/supasin/android/apps/armirror/MainActivity.kt
+++ mobile/src/main/java/me/tatiyanupanwong/supasin/android/apps/armirror/MainActivity.kt
@@ -1,13 +1,19 @@
 package me.tatiyanupanwong.supasin.android.apps.armirror
 
+import android.Manifest.permission.CAMERA
+import android.app.Activity
 import android.app.ActivityManager
 import android.content.Context
+import android.content.pm.PackageManager.PERMISSION_GRANTED
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
 import android.widget.Toast.LENGTH_LONG
 
 private const val MIN_OPENGL_VERSION = 3.0
+private const val REQ_CAMERA_PERMISSION = 0x123
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,6 +27,18 @@ class MainActivity : AppCompatActivity() {
         }
 
         setContentView(R.layout.activity_main)
+
+        requestCameraPermission(this, REQ_CAMERA_PERMISSION)
+    }
+
+    override fun onRequestPermissionsResult(
+            requestCode: Int, permissions: Array<String>, results: IntArray) {
+
+        if (!hasCameraPermission(this)) {
+            Toast.makeText(this, "Camera permission is needed to run this application", LENGTH_LONG)
+                    .show()
+            finish()
+        }
     }
 
     private fun isOpenGlSupported(context: Context): Boolean {
@@ -29,5 +47,13 @@ class MainActivity : AppCompatActivity() {
                 .glEsVersion
 
         return openGlVersionString.toDouble() >= MIN_OPENGL_VERSION
+    }
+
+    private fun requestCameraPermission(activity: Activity, requestCode: Int) {
+        ActivityCompat.requestPermissions(activity, arrayOf(CAMERA), requestCode)
+    }
+
+    private fun hasCameraPermission(context: Context): Boolean {
+        return ContextCompat.checkSelfPermission(context, CAMERA) == PERMISSION_GRANTED
     }
 }
